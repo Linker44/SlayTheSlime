793a5e8c1b3c07280439106f2f34d2b681b2fa59
Linker <falopezjanza@gmail.com>
2024-05-17 12:58:44 -0300

    feature: improved enemy colision detection

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,14 +5,16 @@ const SPEED = 60
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var ray_cast_down_left = $RayCastDownLeft
+@onready var ray_cast_down_right = $RayCastDownRight
 
 var direction = 1
 
-func _process(delta):
-	if ray_cast_right.is_colliding():
+func _physics_process(delta):
+	if ray_cast_right.is_colliding() or not ray_cast_down_right.is_colliding():
 		direction = -1
 		animated_sprite_2d.flip_h = true
-	if ray_cast_left.is_colliding():
+	if ray_cast_left.is_colliding() or not ray_cast_down_left.is_colliding():
 		direction = 1
 		animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
